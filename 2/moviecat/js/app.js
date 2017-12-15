bedabb1a6dcd582493fe3fffe9b9961cafe283fe
zhaohuan <1987287033@qq.com>
2017-12-15 20:24:15 +0800

    finish

--- 2/moviecat/js/app.js
+++ 2/moviecat/js/app.js
@@ -1,49 +1,45 @@
-angular.module('app',[
-	'ui.router',
-	'route',
-	'public',
-	'in_theatersModule',
-	'coming_soonModule'
+angular.module('app', [
+    'ui.router',
+    'route',
+    'public',
+    'in_theatersModule',
+    'coming_soonModule',
+    'topModule'
 ])
 
-	
-
-	.controller('searchCtrl',['$stateParams','myService','$scope',function($stateParams,myService,$scope){
-	
-		myService.myJsonp(myService.url + '/v2/movie/search',{
-			q:$stateParams.keyword
-		},function(res){
-
-			console.log(res);
-
-			$scope.result = res;
-
-			$scope.$apply();
-
-		})
-
-	}])
-
-	.controller('navBarCtrl',['$scope','$location',function($scope,$location){
-
-		$scope.searchFn = function(){
-
-			if(!$scope.keyword){
-
-				alert('请输入搜索关键字');
-
-				return;
-			}
-
-			// 跳转到搜索结果页面并且将用户输入的内容传递过去
-			$location.path('/search/'+$scope.keyword);
 
 
-		}
+.controller('searchCtrl', ['$stateParams', 'myService', '$scope', function($stateParams, myService, $scope) {
 
-	}])
+    myService.myJsonp(myService.url + '/v2/movie/search', {
+        q: $stateParams.keyword
+    }, function(res) {
+
+        console.log(res);
+
+        $scope.result = res;
+
+        $scope.$apply();
+
+    })
+
+}])
+
+.controller('navBarCtrl', ['$scope', '$location', function($scope, $location) {
+
+    $scope.searchFn = function() {
+
+        if (!$scope.keyword) {
+
+            alert('请输入搜索关键字');
+
+            return;
+        }
+
+        // 跳转到搜索结果页面并且将用户输入的内容传递过去
+        $location.path('/search/' + $scope.keyword);
 
 
+    }
 
-
-
+}])
